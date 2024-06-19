f594011f8b31ceca856fefd58711bfbf5f2eb666
pablo,css <juanrubenc78@gmail.com>
2024-06-19 12:23:53 -0600

    add folder

---
+++ bloque_01/01_introduccion.py
@@ -0,0 +1,38 @@
+#Ejercicios y ejemplos de las sesiones
+
+#>>> TIPOS DE DATOS
+
+"""
+* Los datos en programación, son estructuras que clasifican la información que nos sera util. En cada lenguaje se construyen y se definen de diferentes formas.
+* En python existen muchas de estas, pero las mas importantes son estas:
+"""
+
+#? STRING | str | cadena de caracteres
+#Es quel contenido que esta en comillas dobles o simples
+"Vivo en 'el Salvador'"
+'Vivo en "el Salvador"'
+
+#? Number
+10,155,4540,122222222,341331231311123,1234445 #INT | integer | enteros
+10.24,5500.1456,3545.2143435,1535.21435 #float | flotantes
+
+#? Booleanos
+True, False # Verdadero | falso #--> boolean
+
+#? None
+None # ---> nada
+
+#? List | Tuple
+["Hola","me","llamo","pablo"] # lista
+[2130,123014,12554,"pablos",True,False,None] #lista
+
+("hola",123,True) # Tupla | tuple
+
+#? Dict
+{
+    "Nombre":"pablo",
+    "edad":19,
+}
+
+# Set | conjunto
+{}
