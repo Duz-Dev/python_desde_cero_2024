d25c73a57ff7ea4f9be5548d3c6491289369a231
pablo,css <juanrubenc78@gmail.com>
2024-06-20 21:37:02 -0600

    update repo

--- actividades/actividad_01.py
+++ actividades/actividad_01.py
@@ -1,4 +1,3 @@
-#!/c/Users/miner/AppData/Local/Programs/Python/Python312/python
 """
 !Realiza un algoritmo para calcular el sueldo de un operador de tal manera que se otorgara un aumento del 5% si el operador trabaja mas de 40 horas. Se tiene como datos:
 ? nombre del empleado
