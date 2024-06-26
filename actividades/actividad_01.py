15dc2b5325351dd813bce02e1b70caac3342e0b6
pablo,css <juanrubenc78@gmail.com>
2024-06-25 22:22:09 -0600

    update actividades

--- actividades/actividad_01.py
+++ actividades/actividad_01.py
@@ -5,32 +5,3 @@
 ? pago por horas trabajadas
 >>>Imprimir el nombre del operador y su sueldo.
 """
-
-#*variables
-#La notación a continuación no es necesaria. Se le conoce como declaración explicita
-nombre: str = "" 
-horas: float = 0.0 #horas trabajadas
-pago_hora: float = 0.0
-sueldo: float = 0.0
-sueldo_aumento: float = 0.0
-sueldo_neto:float = 0.0
-
-#*input
-print(">>> DATOS DEL EMPLEADO <<<")
-nombre = input("Ingresa el nombre: ")
-horas = float(input("Ingresa las horas: "))
-pago_hora = float(input("Ingresa el pago por hora: "))
-
-#*process
-sueldo = pago_hora * horas
-
-if horas > 40:
-    sueldo_aumento = sueldo * 0.05
-
-sueldo_neto = sueldo + sueldo_aumento #Sueldo final a mostrar
-
-#*output
-print("----------------------------")
-print("Nombre del operador:",nombre)
-print("sueldo:\t",sueldo_neto)
-print("----------------------------")
