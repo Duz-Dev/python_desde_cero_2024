d42528f57429a27cfd4f9e02a4df73b8cf74df3c
pablo,css <juanrubenc78@gmail.com>
2024-12-02 21:08:14 -0600

    update

--- actividades/4_excepciones/09.py
+++ actividades/4_excepciones/09.py
@@ -1,6 +1,6 @@
 """ 
 !Ejercicio: Verificación de Archivo de Configuración
-Descripción: Imagina que tu programa depende de un archivo de configuración llamado config.txt. Crea un programa que intente abrir este archivo y valide que contenga ciertos datos necesarios para que el programa funcione correctamente.
+Descripción: Imagina que tu programa depende de un archivo de configuración llamado config.txt. Crea un programa que intente abrir este archivo y valide que contenga ciertos datos necesarios para que el programa funcione correctamente. El archivo debe contener el texto clave=valor
 
 Si el archivo config.txt no existe, el programa debe capturar la excepción FileNotFoundError y crear un archivo nuevo con datos predeterminados.
 Si el archivo existe pero está vacío o incompleto, el programa debe capturar una excepción personalizada llamada ConfiguracionInvalidaError.
--- actividades/4_excepciones/10.py
+++ actividades/4_excepciones/10.py
@@ -7,7 +7,6 @@ Comentario: Debe tener al menos 15 caracteres. Si el comentario es demasiado cor
 ID del producto: Debe ser un número entero positivo. Si el ID no es válido, lanza una excepción IDProductoInvalido.
 Tareas:
 
-Escribe una función que tome la puntuación, comentario y ID del producto como entrada y valide cada campo.
 Utiliza try, except, y finally para gestionar errores y, al final, confirmar si la reseña fue guardada.
 Almacena las reseñas válidas en una lista reseñas.
 Salida esperada:
